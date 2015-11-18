844cd1e3c1d57aa3366d61efedee656ba23e8740
koziarskim <koziarskim@yahoo.com>
2015-11-18 14:07:52 -0600

    Index, login, patient, note, main.js

---
+++ src/main/resources/public/main.js
@@ -0,0 +1,77 @@
+var mainApp = angular.module("mainApp", ['ngRoute']);
+
+mainApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/login', {
+            templateUrl: 'login.html',
+            controller: 'LoginController'
+        })
+        .when('/patient', {
+            templateUrl: 'patient.html',
+            controller: 'PatientController'
+        })
+        .when('/note', {
+            templateUrl: 'note.html',
+            controller: 'NoteController'
+        })
+        .otherwise({
+            redirectTo: '/login'
+        });
+});
+
+mainApp.controller('StudentController', function ($scope) {
+    $scope.students = [
+        {name: 'Mark Waugh', city: 'New York'},
+        {name: 'Steve Jonathan', city: 'London'},
+        {name: 'John Marcus', city: 'Paris'}
+    ];
+
+    $scope.message = "Click on the hyper link to view the students list.";
+});
+
+mainApp.controller('LoginController', function ($scope, $location) {
+    $scope.login = {
+        userName: null,
+        password: null
+    };
+    $scope.loginAction = function () {
+        if ($scope.login.userName == null || $scope.login.password == null) {
+            alert("invalid username or password");
+        } else {
+            $location.path("/patient");
+        }
+    }
+});
+
+mainApp.controller('PatientController', function ($scope) {
+    $scope.students = [
+        {name: 'Mark Waugh', city: 'New York'},
+        {name: 'Steve Jonathan', city: 'London'},
+        {name: 'John Marcus', city: 'Paris'}
+    ];
+});
+
+mainApp.controller('NoteController', function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullName = function () {
+        return $scope.firstName + " " + $scope.lastName;
+    };
+    $scope.name;
+    $scope.report = {'fromDate': null, 'toDate': null}
+    $scope.items = [
+        {'id': 'CPP', 'value': 'Chicago-Portage Park'},
+        {'id': 'CPS', 'value': 'Chicago Pediatrics'},
+        {'id': 'PRG', 'value': 'Park Ridge'},
+        {'id': 'SBG', 'value': 'Schaumburg'},
+        {'id': 'CTH', 'value': 'Chicago/Thorek Hospital'}];
+    $scope.value;
+    $scope.generateReport = function () {
+        if ($scope.report.fromDate == null || $scope.report.toDate == null) {
+            alert('from and to date required!');
+        } else {
+            alert('Under Construction...')
+        }
+
+    }
+});
